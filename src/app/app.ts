98193c388e91fef1b3df22c4fa7faa8e17805ffd
yankumg <miriam.yanku@hva.nl>
2025-09-09 22:12:28 +0200

    task: ui changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderView} from './views/header-view/header-view';
 import {HomeView} from './views/home-view/home-view';
 import {PromptGeneratorComponent} from './components/prompt-generator-component/prompt-generator-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderView, HomeView, PromptGeneratorComponent],
+  imports: [RouterOutlet, HomeView, PromptGeneratorComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/views/home-view/home-view.ts
+++ src/app/views/home-view/home-view.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {PromptGeneratorComponent} from '../../components/prompt-generator-component/prompt-generator-component';
+import {ImagePalette} from '../../components/image-palette/image-palette';
 
 @Component({
   selector: 'app-home',
-  imports: [PromptGeneratorComponent],
+  imports: [PromptGeneratorComponent, ImagePalette],
   templateUrl: './home-view.html',
   styleUrl: './home-view.css'
 })
