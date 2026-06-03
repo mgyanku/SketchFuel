c71e21f14d26b50079200d6d13e98e230c6f3699
yankumg <miriam.yanku@hva.nl>
2026-06-03 20:36:29 +0200

    feature: added default text

--- src/app/components/prompt-generator-component/prompt-generator-component.ts
+++ src/app/components/prompt-generator-component/prompt-generator-component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {PromptService} from '../../service/prompt-service/prompt-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-prompt-generator',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './prompt-generator-component.html',
   styleUrl: './prompt-generator-component.css'
 })
